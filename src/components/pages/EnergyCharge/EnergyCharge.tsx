5ade6ae3fc573a91cc41af8654e59a4e80911f54
webdev <haitani@tam-tam.co.jp>
2023-09-15 06:14:24 +0900

    Add: 連続タップでバッテリーをためるロジックを設定

--- src/components/pages/EnergyCharge/EnergyCharge.tsx
+++ src/components/pages/EnergyCharge/EnergyCharge.tsx
@@ -1,37 +1,115 @@
-import { FC, useMemo } from 'react';
+import { FC, useState, useEffect, useMemo } from 'react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { useNavigate } from 'react-router-dom';
 
+import { Button } from 'components/parts/Button';
 import { RootState } from 'store';
+import { updateGenEnergy, updateChargeUnits } from 'store/energy';
 
 import styles from './EnergyCharge.module.scss';
 
+import { GaugeUi } from './components/GaugeUi';
+import { TimeLeftUi } from './components/TimeLeftUi';
+
 export const EnergyCharge: FC = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const { localId } = useSelector((state: RootState) => state.player);
   const { teamList, selectedTeam } = useSelector((state: RootState) => state.team);
+  const { genEnergy, chargeUnits } = useSelector((state: RootState) => state.energy);
 
   // 所属チームの情報
   const myTeam = useMemo(() => {
     return teamList.find(team => team.id === selectedTeam);
   }, [teamList, selectedTeam]);
 
+  // 役職
   const job = useMemo(() => {
     return myTeam?.challenger === localId ? 'Rescuer' : 'Charger';
   }, [myTeam, localId]);
+
+  // 残り時間
+  const [currentTime, setCurrentTime] = useState<number>(0);
+  const limit = 30;
+  const chargeThreshold = 50;
+
+  // カウントダウンの処理
+  useEffect(() => {
+    let timerID: NodeJS.Timeout | undefined;
+    if (currentTime < limit) {
+      timerID = setInterval(() => {
+        setCurrentTime(currentTime => currentTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(timerID!);
+    }
+    return () => {
+      clearInterval(timerID!);
+    };
+  }, [currentTime]);
+
+  // スコアを加算
+  const handleTap = () => {
+    dispatch(updateGenEnergy(genEnergy + 1));
+  };
 
   return (
     <article className={styles.article}>
       <header className={styles.header}>
         <h1>Energy Charge</h1>
       </header>
-      <p className={styles.paragraph}>
-        You're on <strong>{job}</strong> duty!
-      </p>
-      <footer className={styles.footer}></footer>
+
+      <div className={styles.body}>
+        <p className={styles.paragraph}>
+          You're on <strong>{job}</strong> duty!
+        </p>
+        {currentTime < limit ? (
+          <>
+            <div className={styles.console}>
+              <TimeLeftUi currentTime={currentTime} limit={limit} />
+              <p className={styles.genEnergy}>⚡️ {genEnergy}</p>
+            </div>
+            <button type="button" onClick={handleTap} className={styles.tap}>
+              Tap Screen!!
+            </button>
+          </>
+        ) : (
+          <>
+            <h2 className={styles.result}>Batteries Earned</h2>
+            <p>Great job! Send your results to the Rescuers.</p>
+          </>
+        )}
+        <div className={styles.stock}>
+          {Array(Math.floor(genEnergy / chargeThreshold))
+            .fill(0)
+            .map((_, index) => (
+              <span key={index} className={styles.stockItem}>
+                🔋
+              </span>
+            ))}
+          {genEnergy > 0 && (
+            <GaugeUi
+              addClass={[styles.gauge, currentTime >= limit ? styles['--invisible'] : '']}
+              currentValue={Math.floor(((genEnergy % chargeThreshold) / chargeThreshold) * 100)}
+            />
+          )}
+        </div>
+      </div>
+
+      <footer className={styles.footer}>
+        {currentTime >= limit && (
+          <Button
+            handleClick={() => {
+              navigate('/');
+            }}
+          >
+            exit
+          </Button>
+        )}
+      </footer>
     </article>
   );
 };
